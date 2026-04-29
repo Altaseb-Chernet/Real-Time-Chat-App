09d7845c83e11564f94a7f594d0397956a740d0b
altaseb-chernet <altacher227@gmail.com>
2026-04-29 14:51:16 +0300

    chat room application

--- src/ChatApplication.Core/Modules/User/Models/UserStatus.cs
+++ src/ChatApplication.Core/Modules/User/Models/UserStatus.cs
@@ -5,6 +5,7 @@ namespace ChatApplication.Core.Modules.User.Models;
 public class UserStatus
 {
     public string UserId { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
     public UserStatusType Status { get; set; }
     public DateTime LastSeen { get; set; }
 }
