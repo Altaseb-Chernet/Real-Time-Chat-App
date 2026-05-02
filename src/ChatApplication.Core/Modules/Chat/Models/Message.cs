d9b7eee08822ead93d451ede14ae8c2dcb21f94e
altaseb-chernet <altacher227@gmail.com>
2026-05-02 10:52:22 +0300

    feat: add edit functionality to messages

--- src/ChatApplication.Core/Modules/Chat/Models/Message.cs
+++ src/ChatApplication.Core/Modules/Chat/Models/Message.cs
@@ -9,6 +9,8 @@ public class Message : BaseEntity
     public string RoomId { get; set; } = string.Empty;
     public DateTime SentAt { get; set; }
     public bool IsDeleted { get; set; }
+    public bool IsEdited { get; set; }
+    public DateTime? EditedAt { get; set; }
 
     // Navigation properties
     public AppUser? Sender { get; set; }
